d6d160ad653f93ca5a1d3084836a7eed6b46a97d
Shaan Mathur <shaankaranmathur@gmail.com>
2018-03-12 01:06:51 -0700

    Map interfaces for GPS coordinates

---
+++ Monopoly/Monopoly.Android/AndroidMap.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Android.App;
+using Android.Content;
+using Android.Gms.Location;
+using Android.Gms.Common;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+
+namespace Monopoly.Droid {
+    class AndroidMap : IMap {
+
+        public event EventHandler<((double, double), (double, double))> LocationChanged;
+
+        private static Android.Gms.Location.FusedLocationProviderClient fusedLocationProviderClient;
+
+        public AndroidMap() {
+            // TODO : Does this cast fail?
+            fusedLocationProviderClient = LocationServices.GetFusedLocationProviderClient((Android.App.Activity)Plugin.CurrentActivity.CrossCurrentActivity.Current);
+            
+            /*
+            
+            var locationRequest = new LocationRequest()
+                                        .SetInterval(60 * 1000 * 5)
+                                        .SetFastestInterval(60 * 1000 * 1)
+                                        .SetPriority(LocationRequest.PriorityHighAccuracy);
+            await fusedLocationProviderClient.RequestLocationUpdatesAsync(locationRequest, this);
+            
+            */
+        }
+
+        public async Task<(double, double)> GetCurrentCoordinates() {
+            var location = await fusedLocationProviderClient.GetLastLocationAsync();
+            if (location == null)
+                return (0, 0);
+            System.Diagnostics.Debug.WriteLine($"Acquired (latitude, longitude) = ({location.Latitude},{location.Longitude})");
+            return (location.Latitude, location.Longitude);
+        }
+    }
+}
---
+++ Monopoly/Monopoly/IMap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace Monopoly
+{
+    /* @interface IMap
+     * 
+     * @brief This interface should have implementations for each OS platform to get
+     * essential GPS information required for this application
+     * 
+     */
+    public interface IMap
+    {
+        // @returns a (double, double) representing the device's current GPS coordinates
+        Task<(double, double)> GetCurrentCoordinates();
+
+        /* An event that that will notify all subscribers that the current location 
+         * has changed.
+         * 
+         * @param ((double, double), (double, double))      A pair of GPS coordinates
+         *                                                  representing old location
+         *                                                  and new location, respectively
+         */
+        event EventHandler<((double, double), (double, double))> LocationChanged;
+    }
+}
