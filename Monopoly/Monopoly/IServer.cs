f559f1f0cd44201ac8aacb17e52cff237680d23f
Shaan Mathur <shaankaranmathur@gmail.com>
2018-03-13 03:30:29 -0700

    Generic interface for what a server connection should look like

---
+++ Monopoly/Monopoly/IServer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json.Linq;
+
+namespace Monopoly
+{
+    public interface IServer
+    {
+        // Connects to the server by opening the connection
+        bool Connect();
+
+        // Connects to server asynchronously
+        Task<bool> ConnectAsync();
+
+        // Send raw data to the server, with no response expected. 
+        void Send(JObject json);
+
+        // Send a request to the server and get a response asynchronously
+        Task<string> Request(JObject json);
+
+        // Closes connection to the server synchronously
+        void Close();
+
+        // Closes connection to the server asynchronously
+        Task<bool> CloseAsync();
+    }
+}
