b22f6c001ca34e3020e0fc823b450f032b2d4ba8
Shaan Mathur <shaankaranmathur@gmail.com>
2018-03-13 03:31:23 -0700

    Represents player credits

---
+++ Monopoly/Monopoly/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monopoly
+{
+    public class BankAccount
+    {
+        public long Credits { get; protected set; }
+
+        public BankAccount(long credits = 0) {
+            this.Credits = credits;
+        }
+
+        public bool Withdraw(long credits) {
+            // Must have enough credits
+            if (credits > this.Credits)
+                return false;
+
+            this.Credits -= credits;
+            return true;
+        }
+
+        public bool Deposit(long credits) {
+            this.Credits += credits;
+            return true;
+        }
+
+    }
+}
