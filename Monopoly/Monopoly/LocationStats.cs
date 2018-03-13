dd7e7a2b8570ed8ac205c8a451fb97ff2af8d59a
Shaan Mathur <shaankaranmathur@gmail.com>
2018-03-13 03:32:48 -0700

    General information about a Location. Decoupled from the Location class because Locations are specific instantiations of a particular tier

---
+++ Monopoly/Monopoly/LocationStats.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Newtonsoft.Json.Linq;
+
+namespace Monopoly
+{
+    public class LocationStats
+    {
+        public string Name { get; protected set; }
+
+        // The i^th price corresponds to the i^th tier
+        public long[] Prices { get; protected set; }
+
+        // The i^th tax corresponds to the taxation needed for the i^th tier
+        public long[] Taxes { get; protected set; }
+
+        // (NorthEast, SouthWest) GPS Coordinates to define the region
+        public ((double, double), (double, double)) Corners { get; protected set; }
+
+        // Purchase Code to represent this item
+        public long PurchaseCode { get; protected set; }
+
+        public LocationStats(string json) : this(JObject.Parse(json)) { }
+
+        public LocationStats(JObject json) {
+            // Retrieve values of interest
+            Name = (string)json["name"];
+
+            var northeast = ((string)json["northeast"]).Split(',');
+            var southwest = ((string)json["southwest"]).Split(',');
+            Corners = ((Convert.ToDouble(northeast[0]), Convert.ToDouble(northeast[1])),
+                       (Convert.ToDouble(southwest[0]), Convert.ToDouble(southwest[1])));
+
+            var prices = (JArray)json["prices"];
+            var taxes = (JArray)json["taxes"];
+
+            var size = Math.Min(prices.Count, taxes.Count);
+            Prices = new long[size];
+            Taxes = new long[size];
+
+            // Makes sure Prices.Count == Taxes.Count
+            for (int i = 0; i < size; i++) {
+                Prices[i] = (long)prices[i];
+                Taxes[i] = (long)taxes[i];
+            }
+        }
+    }
+}
